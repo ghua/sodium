c644f586ff611e425cb76a1b35533985fb7843a4
Semyon Velichko <semyon@velichko.net>
2021-07-17 18:42:02 +0300

    add helpers: Bin2base64, Base642bin, Base64EncodedLen

---
+++ helpers.go
@@ -0,0 +1,59 @@
+package sodium
+
+// #cgo pkg-config: libsodium
+// #include <sodium.h>
+// #include <sodium/utils.h>
+// #include <stdlib.h>
+import "C"
+import (
+	"unsafe"
+)
+
+const (
+	sodium_base64_VARIANT_ORIGINAL            = C.sodium_base64_VARIANT_ORIGINAL
+	sodium_base64_VARIANT_ORIGINAL_NO_PADDING = C.sodium_base64_VARIANT_ORIGINAL_NO_PADDING
+	sodium_base64_VARIANT_URLSAFE             = C.sodium_base64_VARIANT_URLSAFE
+	sodium_base64_VARIANT_URLSAFE_NO_PADDING  = C.sodium_base64_VARIANT_URLSAFE
+)
+
+func Base64EncodedLen(binLen uint32, variant int) uint32 {
+	return uint32(C.sodium_base64_encoded_len(C.ulong(binLen), C.int(variant)))
+}
+
+func Bin2base64(binString string, b64String *string, variant int) int {
+	bin := C.CString(binString)
+	binLength := len(binString)
+	b64MaxLength := C.size_t(Base64EncodedLen(uint32(binLength), variant))
+	var b64 C.char
+	defer C.free(unsafe.Pointer(bin))
+
+	//char * sodium_bin2base64(char * const b64, const size_t b64_maxlen,
+	//	const unsigned char * const bin, const size_t bin_len,
+	//	const int variant
+	//)
+	r := C.sodium_bin2base64(&b64, b64MaxLength, (*C.uchar)(unsafe.Pointer(bin)), C.size_t(binLength), C.int(variant))
+
+	*b64String = C.GoString(&b64)
+
+	return int(*r)
+}
+
+func Base642bin(b64 string, binString *string, variant int) int {
+	var b64Length = len(b64)
+	var binLength = b64Length/4*3 + 2
+
+	b64C := C.CString(b64)
+	var bin C.char
+	defer C.free(unsafe.Pointer(b64C))
+
+	//int sodium_base642bin(unsigned char * const bin, const size_t bin_maxlen,
+	//	const char * const b64, const size_t b64_len,
+	//	const char * const ignore, size_t * const bin_len,
+	//	const char ** const b64_end, const int variant
+	//)
+	r := C.sodium_base642bin((*C.uchar)(unsafe.Pointer(&bin)), C.ulong(binLength), b64C, C.ulong(b64Length), nil, nil, nil, C.int(variant))
+
+	*binString = C.GoString(&bin)
+
+	return int(r)
+}
---
+++ helpers_test.go
@@ -0,0 +1,81 @@
+package sodium
+
+import (
+	"io/ioutil"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+type testInput struct {
+	Clear   string
+	Base64  string
+	Variant int
+}
+
+var licenceContent, _ = ioutil.ReadFile("LICENSE")
+var testInputs = []testInput{
+	{
+		Clear:   "test",
+		Base64:  "dGVzdA==",
+		Variant: sodium_base64_VARIANT_URLSAFE,
+	},
+	{
+		Clear: string(licenceContent),
+		Base64: "VGhlIE1JVCBMaWNlbnNlIChNSVQpCgpDb3B5cmlnaHQgKGMpIDIwMTYgSmFtZXMgUnVhbiA8cnVhbmJlaWhvbmdAZ21haWwuY29tP" +
+			"goKUGVybWlzc2lvbiBpcyBoZXJlYnkgZ3JhbnRlZCwgZnJlZSBvZiBjaGFyZ2UsIHRvIGFueSBwZXJzb24gb2J0YWluaW5nIGEgY2" +
+			"9weQpvZiB0aGlzIHNvZnR3YXJlIGFuZCBhc3NvY2lhdGVkIGRvY3VtZW50YXRpb24gZmlsZXMgKHRoZSAiU29mdHdhcmUiKSwgdG8" +
+			"gZGVhbAppbiB0aGUgU29mdHdhcmUgd2l0aG91dCByZXN0cmljdGlvbiwgaW5jbHVkaW5nIHdpdGhvdXQgbGltaXRhdGlvbiB0aGUg" +
+			"cmlnaHRzCnRvIHVzZSwgY29weSwgbW9kaWZ5LCBtZXJnZSwgcHVibGlzaCwgZGlzdHJpYnV0ZSwgc3VibGljZW5zZSwgYW5kL29yI" +
+			"HNlbGwKY29waWVzIG9mIHRoZSBTb2Z0d2FyZSwgYW5kIHRvIHBlcm1pdCBwZXJzb25zIHRvIHdob20gdGhlIFNvZnR3YXJlIGlzCm" +
+			"Z1cm5pc2hlZCB0byBkbyBzbywgc3ViamVjdCB0byB0aGUgZm9sbG93aW5nIGNvbmRpdGlvbnM6CgpUaGUgYWJvdmUgY29weXJpZ2h" +
+			"0IG5vdGljZSBhbmQgdGhpcyBwZXJtaXNzaW9uIG5vdGljZSBzaGFsbCBiZSBpbmNsdWRlZCBpbiBhbGwKY29waWVzIG9yIHN1YnN0" +
+			"YW50aWFsIHBvcnRpb25zIG9mIHRoZSBTb2Z0d2FyZS4KClRIRSBTT0ZUV0FSRSBJUyBQUk9WSURFRCAiQVMgSVMiLCBXSVRIT1VUI" +
+			"FdBUlJBTlRZIE9GIEFOWSBLSU5ELCBFWFBSRVNTIE9SCklNUExJRUQsIElOQ0xVRElORyBCVVQgTk9UIExJTUlURUQgVE8gVEhFIF" +
+			"dBUlJBTlRJRVMgT0YgTUVSQ0hBTlRBQklMSVRZLApGSVRORVNTIEZPUiBBIFBBUlRJQ1VMQVIgUFVSUE9TRSBBTkQgTk9OSU5GUkl" +
+			"OR0VNRU5ULiBJTiBOTyBFVkVOVCBTSEFMTCBUSEUKQVVUSE9SUyBPUiBDT1BZUklHSFQgSE9MREVSUyBCRSBMSUFCTEUgRk9SIEFO" +
+			"WSBDTEFJTSwgREFNQUdFUyBPUiBPVEhFUgpMSUFCSUxJVFksIFdIRVRIRVIgSU4gQU4gQUNUSU9OIE9GIENPTlRSQUNULCBUT1JUI" +
+			"E9SIE9USEVSV0lTRSwgQVJJU0lORyBGUk9NLApPVVQgT0YgT1IgSU4gQ09OTkVDVElPTiBXSVRIIFRIRSBTT0ZUV0FSRSBPUiBUSE" +
+			"UgVVNFIE9SIE9USEVSIERFQUxJTkdTIElOIFRIRQpTT0ZUV0FSRS4K",
+		Variant: sodium_base64_VARIANT_URLSAFE,
+	},
+}
+
+func TestBase642Bin(t *testing.T) {
+	var binActual string
+
+	for _, v := range testInputs {
+		Base642bin(v.Base64, &binActual, v.Variant)
+		if binActual != v.Clear {
+			t.Fatalf("expected string %s, given: %s", v.Clear, binActual)
+		}
+	}
+}
+
+func TestBin2base64(t *testing.T) {
+	var b64Actual string
+
+	for _, v := range testInputs {
+		Bin2base64(v.Clear, &b64Actual, v.Variant)
+		if b64Actual != v.Base64 {
+			t.Fatalf("expected base64 %s, given: %s", v.Base64, b64Actual)
+		}
+	}
+}
+
+func TestBase64EncodedLen(t *testing.T) {
+	var binLen uint32
+	var b64LenExpected uint32
+	var b64LenActual uint32
+
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for i := 0; i < 100; i++ {
+		binLen = uint32(r.Intn(100))
+		b64LenExpected = (binLen+2)/3*4 + 1
+
+		b64LenActual = Base64EncodedLen(binLen, sodium_base64_VARIANT_URLSAFE)
+		if b64LenExpected != b64LenActual {
+			t.Fatalf("expected len is %d, given: %d", b64LenExpected, b64LenActual)
+		}
+	}
+}
